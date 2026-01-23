68b4e01a6c9a8812e36061a965cf9412346b1200
mariami57 <mmitova3@gmail.com>
2026-01-23 18:03:30 +0200

    added more testing logic to bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,20 @@
-print('Bot is running...')
+import os
+import json
+
+event_path = os.environ['GITHUB_EVENT_PATH']
+
+with open(event_path, 'r') as f:
+    event = json.load(f)
+
+
+comment_text = event['comment']['body']
+comment_author = event['comment']['user']['login']
+issue_number = event['issue']['number']
+repo_name = event['repository']['full_name']
+
+print(f"Comment on {repo_name} issue #{issue_number}")
+print(f"Author: {comment_author}")
+print(f"Text: {comment_text}")
+
+if 'assign me' in comment_text.lower():
+    print(f'Assigning {comment_author} to issue {issue_number}')
