f76148084bbab1037ff01aa06416d86d3f1a0d36
mariami57 <mmitova3@gmail.com>
2026-01-26 20:04:06 +0200

    added label logic

--- bot.py
+++ bot.py
@@ -17,16 +17,34 @@ print(f"Comment on {repo_name} issue #{issue_number}")
 print(f"Author: {comment_author}")
 print(f"Text: {comment_text}")
 
+def ensure_label(repo, name, color='ededed', description=''):
+    try:
+        repo.get_label(name)
+    except Exception:
+        repo.create_label(name=name, color=color, description=description)
+
 if 'assign me' in comment_text.lower():
     gh = Github(auth=Auth.Token(os.environ['GH_TOKEN']))
     repo = gh.get_repo(repo_name)
     issue = repo.get_issue(number=issue_number)
 
+    ensure_label(repo, 'bot:assigned', '0e8a16', 'Assigned by bot')
+    ensure_label(repo, 'bot:dropped', 'b60205', 'Unassigned by bot')
+
+
     try:
         issue.add_to_assignees(comment_author)
+        issue.add_to_labels('bot:assigned')
+
+        existing_labels = [label.name for label in issue.get_labels()]
+        if 'bot:dropped' in existing_labels:
+            issue.remove_from_labels('bot:dropped')
+
         issue.create_comment(f'Assigned to {comment_author}.\n \n'
         f'*This comment was automatically generated*')
         print(f'Assigned to {comment_author} successfully, - issue #{issue_number}\n')
 
     except Exception as e:
         print(f'Error assigning: {e}')
+
+
