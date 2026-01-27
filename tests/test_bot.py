2d322d8f8ebab340575367148a9364695fb60145
mariami57 <mmitova3@gmail.com>
2026-01-27 17:00:38 +0200

    added more tests

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -30,3 +30,34 @@ def test_handle_assign():
     issue.add_to_assignees.assert_called_with('Alice')
     issue.add_to_labels.assert_called_with('bot:assigned')
     issue.create_comment.assert_called()
+
+def test_check_in_reply_by_assignee_assignee_not_in_issue_assignees():
+    issue = fake_issue()
+    label_mock = MagicMock()
+    label_mock.name = 'bot:awaiting-response'
+    issue.get_labels.return_value = [label_mock]
+
+    assert check_in_reply_by_assignee(issue, 'Bob') is False
+
+
+def test_check_in_reply_by_assignee_label_not_in_issue_labels():
+    issue = fake_issue()
+    assignee_mock = MagicMock()
+    assignee_mock.login = 'Bob'
+    issue.assignees = [assignee_mock]
+
+    assert check_in_reply_by_assignee(issue, 'Bob') is False
+
+def test_check_in_reply_by_assignee_label_returns_true():
+    issue = fake_issue()
+    assignee_mock = MagicMock()
+    label_mock = MagicMock()
+    assignee_mock.login = 'Bob'
+    label_mock.name = 'bot:awaiting-response'
+    issue.assignees = [assignee_mock]
+    issue.get_labels.return_value = [label_mock]
+
+    assert check_in_reply_by_assignee(issue, 'Bob') is True
+
+
+
