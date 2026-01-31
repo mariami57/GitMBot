ff9631a614de0fa2aec64017ae5a2b5d6fdc62d2
mariami57 <mmitova3@gmail.com>
2026-01-31 15:12:09 +0200

    added more tests

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -66,6 +66,10 @@ def test_handle_issue_comment_unassign(monkeypatch):
         'repository': {'full_name': 'test/repo'}
     }
 
+    fake_gh = MagicMock()
+    fake_gh.get_repo.return_value = repo
+    monkeypatch.setattr('bot.get_github', lambda: fake_gh)
+
     assignee = MagicMock()
     assignee.login = 'Alice'
     issue.assignees = [assignee]
@@ -81,10 +85,6 @@ def test_handle_issue_comment_unassign(monkeypatch):
 
     issue.get_labels.return_value = [assigned_label]
 
-    fake_gh = MagicMock()
-    fake_gh.get_repo.return_value = repo
-    monkeypatch.setattr('bot.get_github', lambda: fake_gh)
-
     handle_issue_comment(event)
 
     issue.remove_from_assignees.assert_called_with('Alice')
@@ -98,3 +98,40 @@ def test_handle_issue_comment_unassign(monkeypatch):
     issue.create_comment.assert_called()
     assert (f"@{event['comment']['user']['login']} has"
     " unassigned themselves from this issue.\n\n") in issue.create_comment.call_args[0][0]
+
+def test_handle_issue_comment_working(monkeypatch):
+    issue = fake_issue()
+    repo = fake_repo(issue)
+
+    event = {
+        'comment': {'body': '/working', 'user': {'login': 'Alice', 'type': 'User'}},
+        'issue': {'number': 1},
+        'repository': {'full_name': 'test/repo'}
+    }
+
+    fake_gh = MagicMock()
+    fake_gh.get_repo.return_value = repo
+    monkeypatch.setattr('bot.get_github', lambda: fake_gh)
+
+    assignee = MagicMock()
+    assignee.login = 'Alice'
+    issue.assignees = [assignee]
+
+    assigned_label = MagicMock()
+    assigned_label.name = 'bot:assigned'
+
+    checkin_label = MagicMock()
+    checkin_label.name = 'bot:checkin-sent'
+
+    awaiting_label = MagicMock()
+    awaiting_label.name = 'bot:awaiting-response'
+
+    issue.get_labels.return_value = [awaiting_label, assigned_label, checkin_label]
+
+    handle_issue_comment(event)
+
+
+    issue.remove_from_labels.assert_any_call('bot:checkin-sent', 'bot:awaiting-response')
+    issue.create_comment.assert_called()
+    assert (f"Thanks @{event['comment']['user']['login']} for "
+            "confirming you are working on this issue!") in issue.create_comment.call_args[0][0]
